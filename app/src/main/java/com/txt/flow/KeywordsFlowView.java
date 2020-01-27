aed7a14feb824c2622420718ea2bb39510603c7e
蔡凯歌 <caikaige.g@gmail.com>
2020-01-28 01:26:49 +0800

    重构项目

--- app/src/main/java/com/txt/flow/KeywordsFlowView.java
+++ app/src/main/java/com/txt/flow/KeywordsFlowView.java
@@ -1,4 +1,4 @@
-package com.wenhuaijun.keywordsflowdemo;
+package com.txt.flow;
 
 import android.content.Context;
 import android.util.AttributeSet;
@@ -10,7 +10,7 @@ import java.util.Random;
 /**
  * Created by Administrator on 2016/2/24 0024.
  */
-public class KeywordsFlowView extends KeywordsFlow{
+public class KeywordsFlowView extends KeywordsFlow {
     //手势滑动
     public static final int SNAP_VELOCITY = 50;
     private float yDown;
@@ -20,6 +20,7 @@ public class KeywordsFlowView extends KeywordsFlow{
     private boolean shouldScroolFlow = true;
     //用于计算手指滑动的速度。
     private VelocityTracker mVelocityTracker;
+
     public KeywordsFlowView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
@@ -32,17 +33,6 @@ public class KeywordsFlowView extends KeywordsFlow{
         super(context);
     }
 
-
-
-    public void  setWords(String[] words){
-        this.words =words;
-    }
-    public void show(String[] words,int animType){
-        setWords(words);
-        rubKeywords();
-        feedKeywordsFlow(this, words);
-        go2Show(animType);
-    }
     private static void feedKeywordsFlow(KeywordsFlow keywordsFlow, String[] words) {
         Random random = new Random();
         for (int i = 0; i < KeywordsFlow.MAX; i++) {
@@ -51,33 +41,47 @@ public class KeywordsFlowView extends KeywordsFlow{
             keywordsFlow.feedKeyword(tmp);
         }
     }
-    public void setTextShowSize(int size){
-        MAX=size;
+
+    public void setWords(String[] words) {
+        this.words = words;
     }
-    public void shouldScroolFlow(boolean shouldScroolFlow){
-        this.shouldScroolFlow =shouldScroolFlow;
+
+    public void show(String[] words, int animType) {
+        setWords(words);
+        rubKeywords();
+        feedKeywordsFlow(this, words);
+        go2Show(animType);
     }
+
+    public void setTextShowSize(int size) {
+        MAX = size;
+    }
+
+    public void shouldScroolFlow(boolean shouldScroolFlow) {
+        this.shouldScroolFlow = shouldScroolFlow;
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if(!shouldScroolFlow){
-            return  super.onTouchEvent(event);
+        if (!shouldScroolFlow) {
+            return super.onTouchEvent(event);
         }
         createVelocityTracker(event);
-        switch(event.getAction()){
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                yDown=event.getRawY();
+                yDown = event.getRawY();
                 break;
             case MotionEvent.ACTION_MOVE:
-                yMove=event.getRawY();
+                yMove = event.getRawY();
                 break;
             case MotionEvent.ACTION_UP:
-                yUp=event.getRawY();
-                float distance=yUp-yDown;
-                if(distance<-100&& getScrollVelocity() > SNAP_VELOCITY){
+                yUp = event.getRawY();
+                float distance = yUp - yDown;
+                if (distance < -100 && getScrollVelocity() > SNAP_VELOCITY) {
                     rubKeywords();
-                     feedKeywordsFlow(this, words);
+                    feedKeywordsFlow(this, words);
                     go2Show(KeywordsFlow.ANIMATION_OUT);
-                }else if(distance>30&&getScrollVelocity() > SNAP_VELOCITY){
+                } else if (distance > 30 && getScrollVelocity() > SNAP_VELOCITY) {
                     rubKeywords();
                     feedKeywordsFlow(this, words);
                     go2Show(KeywordsFlow.ANIMATION_IN);
@@ -87,12 +91,14 @@ public class KeywordsFlowView extends KeywordsFlow{
         }
         return true;
     }
+
     private void createVelocityTracker(MotionEvent event) {
         if (mVelocityTracker == null) {
             mVelocityTracker = VelocityTracker.obtain();
         }
         mVelocityTracker.addMovement(event);
     }
+
     private int getScrollVelocity() {
         mVelocityTracker.computeCurrentVelocity(1000);
         int velocity = (int) mVelocityTracker.getXVelocity();
--- app/src/main/java/com/txt/flow/MainActivity.java
+++ app/src/main/java/com/txt/flow/MainActivity.java
@@ -1,31 +1,33 @@
-package com.wenhuaijun.keywordsflowdemo;
+package com.txt.flow;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String[] keywords = {"Apple", "Android", "呵呵",
+            "高富帅", "女神", "拥抱", "旅行", "爱情", "屌丝", "搞笑", "暴走漫画", "重邮", "信科",
+            "唯美", "汪星人", "秋天", "雨天", "科幻", "黑夜",
+            "孤独", "星空", "东京食尸鬼", "金正恩", "张全蛋", "东京热", "陈希妍",
+            "明星", "NBA", "马云", "码农", "动漫", "时尚", "熊孩子", "地理", "伤感",
+            "二次元"
+    };
+
     KeywordsFlowView keywordsFlowView;
     Button flow_in;
     Button flow_out;
 
-    public static final String[] keywords = { "Apple", "Android", "呵呵",
-            "高富帅","女神","拥抱","旅行","爱情","屌丝","搞笑","暴走漫画","重邮","信科",
-            "唯美","汪星人","秋天","雨天","科幻","黑夜",
-            "孤独","星空","东京食尸鬼","金正恩","张全蛋","东京热","陈希妍",
-            "明星","NBA","马云","码农","动漫","时尚","熊孩子","地理","伤感",
-            "二次元"
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        flow_in =(Button)findViewById(R.id.flow_in);
-        flow_out =(Button)findViewById(R.id.flow_out);
-        keywordsFlowView = (KeywordsFlowView)findViewById(R.id.keywordsFlowView);
+        flow_in = findViewById(R.id.flow_in);
+        flow_out = findViewById(R.id.flow_out);
+        keywordsFlowView = findViewById(R.id.keywordsFlowView);
         //设置每次随机飞入文字的个数
         keywordsFlowView.setTextShowSize(15);
         //设置是否允许滑动屏幕切换文字
@@ -38,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         keywordsFlowView.setOnItemClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,((TextView)v).getText().toString(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, ((TextView) v).getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -46,12 +48,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if(v==flow_in){
+        if (v == flow_in) {
             //文字随机飞入
-            keywordsFlowView.show(keywords,KeywordsFlow.ANIMATION_IN);
-        }else if (v==flow_out){
+            keywordsFlowView.show(keywords, KeywordsFlow.ANIMATION_IN);
+        } else if (v == flow_out) {
             //文字随机飞出
-            keywordsFlowView.show(keywords,KeywordsFlow.ANIMATION_OUT);
+            keywordsFlowView.show(keywords, KeywordsFlow.ANIMATION_OUT);
         }
     }
 }
